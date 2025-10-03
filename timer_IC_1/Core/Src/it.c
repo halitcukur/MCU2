28a0739e6a9aedb4f457d76ab52cabf5ab036efa
coders club <hi@codersclub.co>
2025-10-03 19:18:09 +0300

    timer

--- timer_IC_1/Core/Src/it.c
+++ timer_IC_1/Core/Src/it.c
@@ -7,7 +7,22 @@
 
 #include "main.h"
 
+extern TIM_HandleTypeDef htimer2;
+extern TIM_HandleTypeDef htimer5;
+
 void SysTick_Handler(void)
 {
-	//HAL_IncTick();
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
+void TIM2_IRQHandler(void)
+{
+	HAL_TIM_IRQHandler(&htimer2);
+}
+
+void TIM5_IRQHandler(void)
+{
+	HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
+	HAL_TIM_IRQHandler(&htimer5);
 }
--- timer_IC_1/Core/Src/msp.c
+++ timer_IC_1/Core/Src/msp.c
@@ -12,7 +12,38 @@ void HAL_MspInit(void)
 	__HAL_RCC_SYSCFG_CLK_ENABLE();
 	__HAL_RCC_PWR_CLK_ENABLE();
 
-	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+	SCB->SHCSR |= 0x7 << 16;
+
+	HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
+	HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
+}
+
+void HAL_TIM_IC_MspInit(TIM_HandleTypeDef *htim)
+{
+	GPIO_InitTypeDef tim2ch1_gpio = {0};
+
+	__HAL_RCC_TIM2_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	tim2ch1_gpio.Pin = GPIO_PIN_0;
+	tim2ch1_gpio.Mode = GPIO_MODE_AF_PP;
+	tim2ch1_gpio.Alternate = GPIO_AF1_TIM2;
+
+	HAL_GPIO_Init(GPIOA, &tim2ch1_gpio);
+
+	HAL_NVIC_SetPriority(TIM2_IRQn, 15, 0);
+	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim)
+{
+	__HAL_RCC_TIM5_CLK_ENABLE();
+
+	HAL_NVIC_SetPriority(TIM5_IRQn, 15, 0);
+	HAL_NVIC_EnableIRQ(TIM5_IRQn);
 }
 
 void HAL_UART_MspInit(UART_HandleTypeDef *huart)
